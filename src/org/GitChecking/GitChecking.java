2fa58f2dc07de6deb8bdfac331609c2f92853ce6
Vishnu19S <140072334+Vishnu19S@users.noreply.github.com>
2023-07-24 11:46:59 +0530

    Checking New Git

---
+++ src/org/GitChecking/GitChecking.java
@@ -0,0 +1,66 @@
+package org.GitChecking;
+
+public class GitChecking {
+public static void main(String[] args) {
+		
+		String WeekName = "sunday";	
+				
+	switch(WeekName)
+	{
+	case "sunday": System.out.println("1");break;
+	case "Tuesday": System.out.println("2");break;
+	case "Wednesday": System.out.println("3");break;
+	case "Monday": System.out.println("5");break;
+	case "Friday": System.out.println("7");break;
+	case "Thursday": System.out.println("6");break;
+	case "saturday": System.out.println("4");break;
+	default:System.out.println("None");
+	
+		}
+    	
+		/*int num=-9; 
+		
+		if(num>0)
+		{
+			System.out.println("positive");
+		}
+		else if(num < 0)
+		{
+			System.out.println("Negative");
+		}
+		else
+		{
+			System.out.println("Zero");
+		}*/
+		
+		
+		// While
+		
+		/*int i = 1;
+		
+		while(i<=10)
+		{
+			System.out.println(i);
+			i=i+2;
+			
+		}*/
+		
+		/*int i=100;
+		do
+		{
+			System.out.println(i);
+			i++;
+		}
+		while (i<=10);
+		*/
+		
+		
+		for(int i=1 ; i<5 ; i++)
+		{
+			System.out.println(i);
+		}
+		
+	}
+
+
+}
